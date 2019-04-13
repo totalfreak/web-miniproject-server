1759ff452f67dee1d5606ef60b629ad8f26101f3
Totalfreak <dgkartin@live.dk>
2019-04-13 14:05:03 +0200

    Added default html

--- app/routes.js
+++ app/routes.js
@@ -11,4 +11,9 @@ module.exports = function(router) {
         posts.getAllPosts(req,res);
     });
 
+    router.route('*').get(function(req, res) {
+        res.sendfile('../public/index.html'); // load our public/index.html file
+
+    });
+
 };
