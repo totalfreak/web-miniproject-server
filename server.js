85615f292de4496843f1db4bdc6d1a29df30e510
Totalfreak <dgkartin@live.dk>
2019-04-13 14:01:08 +0200

    Server things

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 4500;
 mongoose.connect('mongodb://daniel:web-miniproject@web-miniproject-csnyv.mongodb.net/emberData?retryWrites=true');
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://web-miniproject-server.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://web-miniproject-client.herokuapp.com');
   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     next();
