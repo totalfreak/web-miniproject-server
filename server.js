c7556fde6bb55d92401556914ab0606c58802803
Totalfreak <dgkartin@live.dk>
2019-04-13 15:35:34 +0200

    Should work from local now

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var port = process.env.PORT || 4500;
 mongoose.connect('mongodb+srv://daniel:web-miniproject@web-miniproject-csnyv.mongodb.net/emberData?retryWrites=true');
 
 app.use(function(req, res, next) {
+    //res.setHeader('Access-Control-Allow-Origin', 'https://web-miniproject-client.herokuapp.com');
     res.setHeader('Access-Control-Allow-Origin', '213.32.243.78');
     res.header("default-src", 'none');
   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
