5baebaed6de17567955011c86a4004322c535dbe
Totalfreak <dgkartin@live.dk>
2019-04-14 10:13:21 +0200

    Back to dev

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var port = process.env.PORT || 4500;
 mongoose.connect('mongodb+srv://daniel:web-miniproject@web-miniproject-csnyv.mongodb.net/emberData?retryWrites=true');
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://web-miniproject-client.herokuapp.com');
-    //res.setHeader('Access-Control-Allow-Origin', '*');
+    //res.setHeader('Access-Control-Allow-Origin', 'https://web-miniproject-client.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.header("default-src", 'none');
   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
